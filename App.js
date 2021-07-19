6c2385c30b0ea39f4f6a633c30fbfc48de086c39
Yovanny Diaz <yovadiaz@MacBook-Pro-de-Yovanny.local>
2021-07-19 15:22:07 -0400

    Added files to first commit

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Alert, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import Constants from 'expo-constants';
 
 export default function App() {
+  const [ubicacion, setUbicacion] = useState({});
+
+  const searchLocation = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== 'granted') {
+      return Alert.alert('No tenemos los permisos necesarios para acceder a tu localización del móvil.')
+    }
+
+    const location = await Location.getCurrentPositionAsync();
+    setUbicacion(location);
+  }
+
+  useEffect(() => {
+    searchLocation()
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map}>
+        { ubicacion.coords
+            ? <Marker
+                coordinate={ubicacion.coords}
+                title="Title Mark"
+                description="Description to Mark"
+              />
+            : null
+        }
+      </MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
-  },
+    marginTop: 25,
+  }
 });
